1be47a0b5d301c06d817428f3c79daffecf54fc7
Vituratic <marcel_kambeck@hotmail.de>
2023-09-16 14:10:41 +0200

    feat: implement data validation

--- src/main/java/com/apress/demo/springblog/controller/PostController.java
+++ src/main/java/com/apress/demo/springblog/controller/PostController.java
@@ -2,15 +2,15 @@ package com.apress.demo.springblog.controller;
 
 import com.apress.demo.springblog.domain.Post;
 import com.apress.demo.springblog.service.PostService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Arrays;
 
 @Controller
 @RequestMapping("/posts")
@@ -31,7 +31,10 @@ public class PostController {
     }
 
     @PostMapping
-    public String addPost(@ModelAttribute("post") Post post) {
+    public String addPost(@ModelAttribute("post") @Valid Post post, Errors errors) {
+        if (errors.hasErrors()) {
+            return "addPost";
+        }
         postService.addPost(post);
         return "redirect:/posts";
     }
